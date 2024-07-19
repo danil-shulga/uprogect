54f910b954459e4808b74b6735ec7d5a042d4d98
Danil Shulga <121140857+danil-shulga@users.noreply.github.com>
2024-07-19 18:06:40 +0300

    Delete test1.ts

--- test1.ts
+++
@@ -1,6 +0,0 @@
-function count(arr: (string | number | object)[]): number {
-  const set = new Set(arr);
-  return set.size;
-}
-
-console.log(count(['a', 'b', 'a', { a: 1 }, { a: 1 }, 'c', 1, 2, 1]));
